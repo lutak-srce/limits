e5a829ae9aee854fc1c7a483600fd13442f87880
Jakov Sosic <jsosic@gmail.com>
2023-06-13 02:21:13 +0200

    Remove validation to support stdlib 9.0

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,19 +13,12 @@ define limits (
   $comment  = undef,
 ) {
 
-  ### Input parameters validation
-  validate_string($domain)
-  validate_re($type, ['soft','hard'], 'Valid values are: soft, hard')
-  validate_re($item, ['core','data','fsize','memlock','nofile','rss','stack','cpu','nproc','as','maxlogins','maxsyslogins','priority','locks','sigpending','msgqueue','nice','rtprio'], 'Valid valuse are: core, data, fsize, memlock, nofile, rss, stack, cpu, nproc, as, maxlogins, maxsyslogins, priority, locks, sigpending, msgqueue, nice, rtprio')
-  validate_string($comment)
-
   if ( $priority ) {
     $limits_title = "${priority}-${title}"
   } else {
     $limits_title = $title
   }
 
-  # The permanent change
   file { "/etc/security/limits.d/${limits_title}.conf":
     ensure  => $ensure,
     owner   => root,
